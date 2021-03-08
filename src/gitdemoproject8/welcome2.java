8c6e247d0d906ef02866c346f20ddd5b132217fc
Megha Mapalagama <meghamapalagama@meghas-mbp.home>
2021-03-08 16:00:49 -0500

    third commit

--- src/gitdemoproject8/welcome2.java
+++ src/gitdemoproject8/welcome2.java
@@ -5,6 +5,8 @@ public class welcome2 {
 	public static void main(String[] args) {
 		
 		System.out.println("git demo project8!......welcome2");
+		
+		System.out.println("welcome to git demo project8!......");
 
 	}
 
